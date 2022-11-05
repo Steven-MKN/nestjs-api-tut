3d509b0801618ac2efc6dc008ac817d853710624
Steven Mokoena <steven@sizananitechnologies.com>
2022-11-05 23:33:39 +0200

    setup signup logic, dto validation, updated tables to include relationships

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  signin() {}
+  constructor(private prisma: PrismaService) {}
 
-  signup() {}
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  signin() {}
 }
